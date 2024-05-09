2a88febd35b019cc8cba0bc61614da277b34e412
Cigno05 <paolocalcagni.82@gmail.com>
2024-05-09 14:11:31 +0200

    ADD 2 instance in Production

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-class production
+class Production
 {
 
     // variabili d'istanza
@@ -12,7 +12,7 @@ class production
 
     public function setVote($_vote)
     {
-        if (is_numeric($_vote) && $_vote > 0 && $_vote < 10) {
+        if (is_numeric($_vote) && $_vote > 0 && $_vote <= 10) {
             $this->vote = $_vote;
         } else {
             var_dump('Il Voto non è corretto!');
@@ -20,21 +20,34 @@ class production
         }
     }
 
+    public function setLanguage($_language)
+    {
+        if (strlen($_language) === 3) {
+            $this->language = $_language;
+            // var_dump($_language);
+        }
+    }
 
+    // strtoupper
     // construct
 
-    public function __construct(string $_title, string $_language, int $_vote) {
+    public function __construct(string $_title, string $_language, int $_vote)
+    {
 
         $this->title = $_title;
-        $this->language = $_language;
+        $this->setLanguage(strtoupper($_language));
         $this->setVote($_vote);
     }
 }
 
 
 
+// oggetti production
 
-
+$bttf = new Production('Back to the future', 'eng' , 9);
+// var_dump($bttf);
+$laVitaEBella = new Production('La vita è bella', 'ita', 10);
+// var_dump($laVitaEBella);
 
 
 
