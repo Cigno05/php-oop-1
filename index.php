886b33441a7ef5de09acb175e3adea74c7c931a4
Cigno05 <paolocalcagni.82@gmail.com>
2024-05-09 13:48:32 +0200

    ADD method setVote and construct

--- index.php
+++ index.php
@@ -1,5 +1,36 @@
 <?php
 
+class production
+{
+
+    // variabili d'istanza
+    public $title;
+    public $language;
+    public $vote;
+
+    // metodi 
+
+    public function setVote($_vote)
+    {
+        if (is_numeric($_vote) && $_vote > 0 && $_vote < 10) {
+            $this->vote = $_vote;
+        } else {
+            var_dump('Il Voto non è corretto!');
+
+        }
+    }
+
+
+    // construct
+
+    public function __construct(string $_title, string $_language, int $_vote) {
+
+        $this->title = $_title;
+        $this->language = $_language;
+        $this->setVote($_vote);
+    }
+}
+
 
 
 
